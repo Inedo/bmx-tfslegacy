a5d9d7dec587e1a81428d634fc1e91c9bd21b803
InedoJohn <jrasch@inedo.com>
2018-02-15 17:24:19 -0500

    BuildMaster v6 support

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Inedo.BuildMaster.Extensibility;
+
+[assembly: AssemblyTitle("TFSLegacy")]
+[assembly: AssemblyDescription("Legacy Source Control and Issue Tracking integration for Team Foundation Server. The TFS extension is also required.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyProduct("BuildMaster")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2018")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
+[assembly: RequiredBuildMasterVersion("6.0.0")]
---
+++ README.md
@@ -0,0 +1,5 @@
+## TFS Legacy Components for BuildMaster
+
+This extension contains the legacy components (legacy plan actions, etc.) for use in BuildMaster v6.0 and later that were formerly included with the [TFS](https://github.com/inedo/inedox-tfs) extension. 
+
+Installations that do not use any [legacy features](https://inedo.com/support/kb/1144/buildmaster-legacy-features) do not need to install this extension.
